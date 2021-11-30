d72202ce106e0bc38d84bab142d3f91924696fc7
luis <luisgarmir@hotmail.com>
2021-11-30 13:56:24 -0800

    4 maps

--- src/color_map.py
+++ src/color_map.py
@@ -12,15 +12,27 @@ data = pickle.load(inputFile)
 map_data = np.load('./Data/map.npy')
 
 # Create figure and axes
-fig, (ax1, ax2) = plt.subplots(1,2)
+figure, axis = plt.subplots(2, 2)
 
-ax1.set_title('Mapa')
-ax1.set_xlabel('x (m)')
-ax1.set_ylabel('y (m)')
+axis[0, 0].set_title(' Map ')
+axis[0, 0].set_xlabel('x (m)')
+axis[0, 0].set_ylabel('y (m)')
+axis[0, 0].imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
 
-# Display the image
-ax2.imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
-ax1.imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
+axis[0, 1].set_title(' Navigability Map ')
+axis[0, 1].set_xlabel('x (m)')
+axis[0, 1].set_ylabel('y (m)')
+axis[0, 1].imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
+
+axis[1, 0].set_title(' Navigability Route ')
+axis[1, 0].set_xlabel('x (m)')
+axis[1, 0].set_ylabel('y (m)')
+axis[1, 0].imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
+
+axis[1, 1].set_title(' Navigability Route Map ')
+axis[1, 1].set_xlabel('x (m)')
+axis[1, 1].set_ylabel('y (m)')
+axis[1, 1].imshow(map_data, cmap='hot', interpolation='nearest', aspect='auto')
 
 origin = 4800,3000
 destination = 5000, 10000
@@ -31,7 +43,9 @@ route = rover.result.path()
 
 for i in route:
     rectangle = patches.Rectangle((i[1][1]*20, i[1][0]*20), 20, 20, fc='#0020ff',alpha=0.8)
-    ax2.add_artist(rectangle)
+    rectangle1 = patches.Rectangle((i[1][1]*20, i[1][0]*20), 20, 20, fc='#0020ff',alpha=0.8)
+    axis[1, 0].add_artist(rectangle)
+    axis[1, 1].add_artist(rectangle1)
 
 for i in range(data.shape[0]):
     for j in range(data.shape[1]):
@@ -39,16 +53,24 @@ for i in range(data.shape[0]):
         # Create a Rectangle patch
         if data[i][j] == 1:
             rectangle = plt.Rectangle((j*20,i*20), 20, 20, fc='#870000',alpha=0.5)
-            ax2.add_artist(rectangle)
+            rectangle1 = plt.Rectangle((j*20,i*20), 20, 20, fc='#870000',alpha=0.5)
+            axis[0, 1].add_artist(rectangle)
+            axis[1, 1].add_artist(rectangle1)
         elif data[i][j] == 2:
             rectangle = patches.Rectangle((j*20,i*20), 20, 20, fc='#ff0000',alpha=0.5)
-            ax2.add_artist(rectangle)
+            rectangle1 = patches.Rectangle((j*20,i*20), 20, 20, fc='#ff0000',alpha=0.5)
+            axis[0, 1].add_artist(rectangle)
+            axis[1, 1].add_artist(rectangle1)
         elif data[i][j] == 3:
             rectangle = plt.Rectangle((j*20,i*20), 20, 20, fc='#4aff00',alpha=0.5)
-            ax2.add_artist(rectangle)
+            rectangle1 = plt.Rectangle((j*20,i*20), 20, 20, fc='#4aff00',alpha=0.5)
+            axis[0, 1].add_artist(rectangle)
+            axis[1, 1].add_artist(rectangle1)
         elif data[i][j] == 4:
             rectangle = patches.Rectangle((j*20,i*20), 20, 20, fc='#268200',alpha=0.5)
-            ax2.add_artist(rectangle)
+            rectangle1 = patches.Rectangle((j*20,i*20), 20, 20, fc='#268200',alpha=0.5)
+            axis[0, 1].add_artist(rectangle)
+            axis[1, 1].add_artist(rectangle1)
         else:
             pass
     
